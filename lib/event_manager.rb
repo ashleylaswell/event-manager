3f7c7e65da7690a18dc9bae5ddbbfc9a91b36dbf
Ashley Laswell <lswl9898@gmail.com>
2020-11-06 17:04:05 -0500

    add pseudocode

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -5,5 +5,10 @@ contents = CSV.open "event_attendees.csv", headers: true, header_converters: :sy
 contents.each do |row|
 	name = row[:first_name]
 	zipcode = row[:zipcode]
+
+	#if the zip code is exactly five digits, assume it is ok
+	#if the zip code is more than five digits, truncate it to the first five digits
+	#if the zip code is less than five digits, and zeros to front until it is five digits
+
 	puts "#{name} #{zipcode}"
 end
